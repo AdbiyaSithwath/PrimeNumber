fbab30b1a88f4da08328726a32c0dad2b092a537
AdbiyaSithwath <66269369+AdbiyaSithwath@users.noreply.github.com>
2020-06-23 20:45:24 +0530

    PrimeNumber.java
    This file prints an output as "3 is a prime number"
    
    Thank you!

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,22 @@
-
+public class PrimeNumber{ 
+public static void main(String[] args){
+int i,a=0,flag=0; 
+int b=3; 
+a=b/2;
+if(b==0||b==1){
+System.out.println(b+" is not a prime number");
+} 
+else{ 
+for(i=2;i<=a;i++){ 
+if(b%i==0){ 
+System.out.println(b+" is not a prime number");
+flag=1;
+break;
+} 
+}
+If(flag==0){ 
+System.out.println(b+" is a prime number");
+}
+}
+}
+}
